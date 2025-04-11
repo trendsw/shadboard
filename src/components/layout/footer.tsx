a713ae5bbb050c7a0f6695d9ce6661206d218aa2
trendsw <romankimak69@gmail.com>
2025-04-12 00:21:51 +0300

    Refactor Footer component structure for improved layout and styling consistency

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -6,31 +6,33 @@ export function Footer() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className="container flex justify-between items-center bg-background p-4 md:px-6">
-      <p className="text-xs text-muted-foreground md:text-sm">
-        © {currentYear}{" "}
-        <a
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={cn(buttonVariants({ variant: "link" }), "inline p-0")}
-        >
-          Shadboard
-        </a>
-        .
-      </p>
-      <p className="text-xs text-muted-foreground md:text-sm">
-        Designed & Developed by{" "}
-        <a
-          href="https://github.com/Qualiora"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={cn(buttonVariants({ variant: "link" }), "inline p-0")}
-        >
-          Qualiora
-        </a>
-        .
-      </p>
+    <footer className="bg-background border-t border-sidebar-border">
+      <div className="container flex justify-between items-center p-4 md:px-6">
+        <p className="text-xs text-muted-foreground md:text-sm">
+          © {currentYear}{" "}
+          <a
+            href="/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={cn(buttonVariants({ variant: "link" }), "inline p-0")}
+          >
+            Shadboard
+          </a>
+          .
+        </p>
+        <p className="text-xs text-muted-foreground md:text-sm">
+          Designed & Developed by{" "}
+          <a
+            href="https://github.com/Qualiora"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={cn(buttonVariants({ variant: "link" }), "inline p-0")}
+          >
+            Qualiora
+          </a>
+          .
+        </p>
+      </div>
     </footer>
   )
 }
