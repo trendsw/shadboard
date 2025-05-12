a8b0480dd1599530a2511823dcef7e7b7b27dee9
trendsw <romankimak69@gmail.com>
2025-05-12 12:48:09 +0300

    refactor: replace withAuth() with getToken() in the middleware

--- full-kit/src/middleware.ts
+++ full-kit/src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
-import { withAuth } from "next-auth/middleware"
+import { getToken } from "next-auth/jwt"
 
-import type { NextRequestWithAuth } from "next-auth/middleware"
+import type { NextRequest } from "next/server"
 
 import { isGuestRoute, isPublicRoute } from "@/lib/auth-routes"
 import {
@@ -12,7 +12,7 @@ import {
 } from "@/lib/i18n"
 import { ensureRedirectPathname, ensureWithoutPrefix } from "@/lib/utils"
 
-function redirect(pathname: string, request: NextRequestWithAuth) {
+function redirect(pathname: string, request: NextRequest) {
   const { search, hash } = request.nextUrl
   let resolvedPathname = pathname
 
@@ -31,61 +31,53 @@ function redirect(pathname: string, request: NextRequestWithAuth) {
   return NextResponse.redirect(redirectUrl)
 }
 
-export default withAuth(
-  async function middleware(request: NextRequestWithAuth) {
-    const { pathname } = request.nextUrl
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl
+  const locale = getLocaleFromPathname(pathname)
+  const pathnameWithoutLocale = ensureWithoutPrefix(pathname, `/${locale}`)
+  const isNotPublic = !isPublicRoute(pathnameWithoutLocale)
 
-    const locale = getLocaleFromPathname(pathname)
-    const pathnameWithoutLocale = ensureWithoutPrefix(pathname, `/${locale}`)
-    const isNotPublic = !isPublicRoute(pathnameWithoutLocale)
+  // Handle authentication for protected and guest routes
+  if (isNotPublic) {
+    const token = await getToken({ req: request })
+    const isAuthenticated = !!token
+    const isGuest = isGuestRoute(pathnameWithoutLocale)
+    const isProtected = !isGuest
 
-    // Handle authentication for protected and guest routes
-    if (isNotPublic) {
-      const isAuthenticated = !!request.nextauth.token
-      const isGuest = isGuestRoute(pathnameWithoutLocale)
-      const isProtected = !isGuest
-
-      // Redirect authenticated users away from guest routes
-      if (isAuthenticated && isGuest) {
-        return redirect(process.env.HOME_PATHNAME || "/", request)
-      }
-
-      // Redirect unauthenticated users from protected routes to sign-in
-      if (!isAuthenticated && isProtected) {
-        let redirectPathname = "/sign-in"
-
-        // Maintain the original path for redirection
-        if (pathnameWithoutLocale !== "") {
-          redirectPathname = ensureRedirectPathname(redirectPathname, pathname)
-        }
-
-        return redirect(redirectPathname, request)
-      }
+    // Redirect authenticated users away from guest routes
+    if (isAuthenticated && isGuest) {
+      return redirect(process.env.HOME_PATHNAME || "/", request)
     }
 
-    // Redirect to localized URL if the pathname is missing a locale
-    if (!locale) {
-      return redirect(pathname, request)
+    // Redirect unauthenticated users from protected routes to sign-in
+    if (!isAuthenticated && isProtected) {
+      let redirectPathname = "/sign-in"
+
+      // Maintain the original path for redirection
+      if (pathnameWithoutLocale !== "") {
+        redirectPathname = ensureRedirectPathname(redirectPathname, pathname)
+      }
+
+      return redirect(redirectPathname, request)
     }
-
-    /**
-     * NOTE
-     * If your homepage is not '/', you need to configure a redirect
-     * in next.config.mjs using the redirects() function,
-     * and set the HOME_PATHNAME environment variable accordingly.
-     *
-     * See https://nextjs.org/docs/app/building-your-application/routing/redirecting#redirects-in-nextconfigjs
-     */
-
-    return NextResponse.next()
-  },
-  {
-    callbacks: {
-      // Always authorize requests to ensure the middleware function is called
-      authorized: () => true,
-    },
   }
-)
+
+  // Redirect to localized URL if the pathname is missing a locale
+  if (!locale) {
+    return redirect(pathname, request)
+  }
+
+  /**
+   * NOTE
+   * If your homepage is not '/', you need to configure a redirect
+   * in next.config.mjs using the redirects() function,
+   * and set the HOME_PATHNAME environment variable accordingly.
+   *
+   * See https://nextjs.org/docs/app/building-your-application/routing/redirecting#redirects-in-nextconfigjs
+   */
+
+  return NextResponse.next()
+}
 
 export const config = {
   matcher: [
@@ -96,7 +88,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      * - images folder
-     * - homepage '/'
      * - docs
      */
     "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|images|docs).*)",
